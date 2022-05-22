a67b3021d8ba09091424d7f13355579a855687cb
_lmmmmmm <931420654@qq.com>
2022-05-23 00:01:01 +0800

    chore: path alias config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), 'APP_');
+
   return {
     base: env.APP_BASE_URL,
     plugins: [react()],
-    envPrefix: 'APP_'
+    envPrefix: 'APP_',
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src')
+      }
+    }
   };
 });
