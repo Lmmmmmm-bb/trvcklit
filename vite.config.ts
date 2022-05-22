af826361b841d3f3d2518a573190c5fcfcc9c5e6
_lmmmmmm <931420654@qq.com>
2022-05-22 19:52:04 +0800

    chore: pages env config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), 'APP_');
+  return {
+    base: env.APP_BASE_URL,
+    plugins: [react()],
+    envPrefix: 'APP_'
+  };
 });
