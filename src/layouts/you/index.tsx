3b590944d7c234eefd212d98f4bda1a2ee1ad4f5
_lmmmmmm <931420654@qq.com>
2022-05-24 00:02:14 +0800

    feat: init you layout

--- src/layouts/you/index.tsx
+++ src/layouts/you/index.tsx
@@ -1,7 +1,43 @@
 import { FC } from 'react';
+import { motion } from 'framer-motion';
+import styles from './index.module.scss';
+import CardChecker from '@/components/card-checker';
+import { useChecklist } from '@/common/hooks';
+import { motionProps } from './config';
+import { Divider } from '@douyinfe/semi-ui';
+import { LocalKeysEnum } from '@/common/models';
 
 const You: FC = () => {
-  return <div>you</div>;
+  const { setCheckItem, category } = useChecklist(LocalKeysEnum.You);
+
+  return (
+    <div className={styles.wrapper}>
+      <h3 className={styles.title}>YOU</h3>
+      <motion.div {...motionProps}>
+        {Object.keys(category).map((key) => {
+          const currentCategory = category[key];
+          return (
+            <div key={key}>
+              <Divider className={styles.divider} align='left'>
+                {key}
+              </Divider>
+              <div className={styles.cardWrapper}>
+                {currentCategory.map(({ id, checked, name }) => (
+                  <CardChecker
+                    key={id}
+                    checked={checked}
+                    onClick={() => setCheckItem(id)}
+                  >
+                    {name}
+                  </CardChecker>
+                ))}
+              </div>
+            </div>
+          );
+        })}
+      </motion.div>
+    </div>
+  );
 };
 
 export default You;
