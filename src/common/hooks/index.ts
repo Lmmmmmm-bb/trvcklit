f0e86ee8a9d812f960a8ea4cb4a0bbc5b42876b3
_lmmmmmm <931420654@qq.com>
2022-05-23 18:36:59 +0800

    feat: init me layout

--- src/common/hooks/index.ts
+++ src/common/hooks/index.ts
@@ -1,1 +1,2 @@
 export * from './useSupa';
+export * from './useChecklist';
--- src/layouts/me/index.tsx
+++ src/layouts/me/index.tsx
@@ -1,7 +1,27 @@
 import { FC } from 'react';
+import styles from './index.module.scss';
+import CardChecker from '@/components/card-checker';
+import { useChecklist } from '@/common/hooks';
 
 const Me: FC = () => {
-  return <div>me</div>;
+  const { checklist, setCheckItem } = useChecklist();
+
+  return (
+    <div className={styles.wrapper}>
+      <h3 className={styles.title}>ME</h3>
+      <div className={styles.cardWrapper}>
+        {checklist.map(({ id, name, checked }) => (
+          <CardChecker
+            key={id}
+            checked={checked}
+            onClick={() => setCheckItem(id)}
+          >
+            {name}
+          </CardChecker>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Me;
