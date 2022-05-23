7114760399cf2c6e036603146a00ac333ea89447
_lmmmmmm <931420654@qq.com>
2022-05-24 00:02:03 +0800

    refactor: refactor me layout useChecklist

--- src/layouts/me/index.tsx
+++ src/layouts/me/index.tsx
@@ -4,23 +4,37 @@ import styles from './index.module.scss';
 import CardChecker from '@/components/card-checker';
 import { useChecklist } from '@/common/hooks';
 import { motionProps } from './config';
+import { Divider } from '@douyinfe/semi-ui';
+import { LocalKeysEnum } from '@/common/models';
 
 const Me: FC = () => {
-  const { checklist, setCheckItem } = useChecklist();
+  const { setCheckItem, category } = useChecklist(LocalKeysEnum.Me);
 
   return (
     <div className={styles.wrapper}>
       <h3 className={styles.title}>ME</h3>
-      <motion.div className={styles.cardWrapper} {...motionProps}>
-        {checklist.map(({ id, name, checked }) => (
-          <CardChecker
-            key={id}
-            checked={checked}
-            onClick={() => setCheckItem(id)}
-          >
-            {name}
-          </CardChecker>
-        ))}
+      <motion.div {...motionProps}>
+        {Object.keys(category).map((key) => {
+          const currentCategory = category[key];
+          return (
+            <div key={key}>
+              <Divider className={styles.divider} align='left'>
+                {key}
+              </Divider>
+              <div className={styles.cardWrapper}>
+                {currentCategory.map(({ id, checked, name }) => (
+                  <CardChecker
+                    key={id}
+                    checked={checked}
+                    onClick={() => setCheckItem(id)}
+                  >
+                    {name}
+                  </CardChecker>
+                ))}
+              </div>
+            </div>
+          );
+        })}
       </motion.div>
     </div>
   );
