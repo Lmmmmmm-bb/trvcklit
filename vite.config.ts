017e1452cfc078f4440ff70e1409eafbb6cb0a5a
_lmmmmmm <lmmmmmm12138@gmail.com>
2022-05-26 13:05:47 +0800

    chore: init pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -8,7 +9,7 @@ export default defineConfig(({ mode }) => {
 
   return {
     base: env.APP_BASE_URL,
-    plugins: [react()],
+    plugins: [react(), VitePWA()],
     envPrefix: 'APP_',
     resolve: {
       alias: {
