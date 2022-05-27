56409daf2d051ab51bafaa6c0ffcac4c41ee72ea
_lmmmmmm <lmmmmmm12138@gmail.com>
2022-05-27 21:25:39 +0800

    feat: dark mode will follow the system

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,26 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Switch from '@/layouts/switch';
 import You from '@/layouts/you';
 import Me from '@/layouts/me';
 import { appContext } from './context';
 import { useTheme } from '@/common/hooks';
+import { ThemeModeEnum } from '@/common/models';
 
 const App: FC = () => {
-  const { themeMode, toggleTheme } = useTheme();
+  const { themeMode, toggleTheme, setThemeMode } = useTheme();
+
+  useEffect(() => {
+    matchMedia('(prefers-color-scheme: dark)').addEventListener(
+      'change',
+      (e) => {
+        const isDarkMode = e.matches;
+        isDarkMode
+          ? setThemeMode(ThemeModeEnum.Dark)
+          : setThemeMode(ThemeModeEnum.Light);
+      }
+    );
+  }, []);
 
   return (
     <appContext.Provider value={{ themeMode, toggleTheme }}>
--- src/common/hooks/useTheme.ts
+++ src/common/hooks/useTheme.ts
@@ -5,25 +5,23 @@ import { isSystemDarkMode } from '../utils/system-theme';
 const BODY_THEME_TAG = 'theme-mode';
 
 export const useTheme = () => {
-  const [themeMode, setThemeMode] = useState<ThemeModeEnum>(
+  const [themeMode, setThemeMode] = useState(
     isSystemDarkMode() ? ThemeModeEnum.Dark : ThemeModeEnum.Light
   );
 
   const toggleTheme = () => {
-    const body = document.body;
     const theme =
       themeMode === ThemeModeEnum.Light
         ? ThemeModeEnum.Dark
         : ThemeModeEnum.Light;
 
     setThemeMode(theme);
-    body.setAttribute(BODY_THEME_TAG, theme);
   };
 
   useEffect(() => {
     const body = document.body;
     body.setAttribute(BODY_THEME_TAG, themeMode);
-  }, []);
+  }, [themeMode]);
 
-  return { themeMode, toggleTheme };
+  return { themeMode, setThemeMode, toggleTheme };
 };
